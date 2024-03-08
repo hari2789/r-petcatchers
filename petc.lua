945d2880c82d483559e4b3b35e7d965bceb56081
vyworn <153512249+vyworn@users.noreply.github.com>
2024-03-08 11:52:04 +0100

    Update petc.lua

--- petc.lua
+++ petc.lua
@@ -1,12 +1,17 @@
--- V1.06
+-- V1.08
+
 -- Function to send notifications
 local function KrakenRespawnNotification(title, text)
   game:GetService("StarterGui"):SetCore("SendNotification", {
       Title = tostring(title),
       Text = tostring(text),
-      Duration = 1 -- Adjust the duration as needed
+      Duration = 1
   })
 end
+
+-- Notify when the script is loaded
+KrakenRespawnNotification("Status", "Script Has Been Loaded")
+KrakenRespawnNotification("Kraken Auto Respawn", "Default: Disabled")
 
 -- Script settings
 getgenv().Settings = {
@@ -17,7 +22,7 @@ getgenv().Settings = {
 }
 
 local UIS = game:GetService("UserInputService")
-local RS = game:GetService("ReplicatedStorage")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- Listen for key presses
 UIS.InputBegan:Connect(function(input)
@@ -27,9 +32,9 @@ UIS.InputBegan:Connect(function(input)
       Settings.KrakenRespawn = not Settings.KrakenRespawn
       -- Notify the user about the script state change
       if Settings.KrakenRespawn then
-        KrakenRespawnNotification("Kraken Auto Respawn", "Enabled")
+          KrakenRespawnNotification("Kraken Auto Respawn", "Enabled")
       else
-        KrakenRespawnNotification("Kraken Auto Respawn", "Disabled")
+          KrakenRespawnNotification("Kraken Auto Respawn", "Disabled")
       end
   end
 end)
@@ -38,9 +43,9 @@ end)
 while true do
   if Settings.KrakenRespawn then
       -- Fire the server event to respawn the boss
-      RS:WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild("Remote"):WaitForChild("Event"):FireServer("RespawnBoss", "the-kraken")
-      task.wait(4) -- Wait for 4 seconds before next respawn
+      ReplicatedStorage:WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild("Remote"):WaitForChild("Event"):FireServer("RespawnBoss", "the-kraken")
+      task.wait(4)
   else
-      task.wait(1) -- If script is disabled, wait for 1 second before checking again
+      task.wait(1)
   end
 end
