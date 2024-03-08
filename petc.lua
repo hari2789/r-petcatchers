3f08f23216260b1f46ff70143c37b3190b4dfc9a
vyworn <153512249+vyworn@users.noreply.github.com>
2024-03-08 23:46:53 +0100

    Update petc.lua

--- petc.lua
+++ petc.lua
@@ -1,6 +1,6 @@
 --[[
 
-    Made by av / hari
+    Made by Av & Hari
     Version: v1.8
     Game Version: v1.1.2
 
@@ -8,21 +8,17 @@
     Press K to toggle Black Market auto buy
     Press U to toggle the script on/off
 
-]]
-
-local function InjectedScript()
+]] local function Script()
     -- Settings and default configurations
     local Settings = {
-        Toggles = { 
+        Toggles = {
             ScriptEnabled = true, -- Toggle for enabling/disabling the script
             KrakenRespawn = false, -- Toggle for Kraken respawn
-            BuyBlackMarket = false, -- Toggle for Black Market auto buy
-            BuyGemTrader = false, -- Toggle for Gem Trader auto buy
+            BuyBlackMarket = false -- Toggle for Black Market auto buy
         },
         Keybinds = {
             KrakenToggle = Enum.KeyCode.J, -- Keybind for toggling Kraken respawn
             BlackMarketToggle = Enum.KeyCode.K, -- Keybind for toggling Black Market auto buy
-            GemTraderToggle = Enum.KeyCode.L, -- Keybind for toggling Gem Trader auto buy
             ToggleScript = Enum.KeyCode.U -- Keybind for enabling/disabling the script
         },
         Timers = {
@@ -65,8 +61,8 @@ local function InjectedScript()
         while true do
             if Settings.Toggles.KrakenRespawn and Settings.Toggles.ScriptEnabled then
                 local success, errorMessage = pcall(function()
-                    ReplicatedStorage:WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild(
-                        "Remote"):WaitForChild("Event"):FireServer("RespawnBoss", "the-kraken")
+                    ReplicatedStorage:WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network")
+                        :WaitForChild("Remote"):WaitForChild("Event"):FireServer("RespawnBoss", "the-kraken")
                     wait(Settings.Timers.RespawnCooldown)
                 end)
 
@@ -82,8 +78,8 @@ local function InjectedScript()
     -- Function to buy an item from the Black Market
     local function BuyItem(shop, itemIndex)
         local success, RemoteEvent = pcall(function()
-            return ReplicatedStorage:WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild(
-                "Remote"):WaitForChild("Event")
+            return ReplicatedStorage:WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network")
+                :WaitForChild("Remote"):WaitForChild("Event")
         end)
         if success then
             local result = RemoteEvent:FireServer("BuyShopItem", shop, itemIndex)
@@ -107,52 +103,71 @@ local function InjectedScript()
         end
     end
 
-    -- Function to automatically buy items from the Black Market
-    local function BuyGemTrader()
-        while true do
-            if Settings.Toggles.BuyGemTrader and Settings.Toggles.ScriptEnabled then
-                for i = 1, 3 do
-                    BuyItem("gem-shop", i)
-                    wait(Settings.Timers.BuyDelay)
-                end
-                wait(Settings.Timers.BuyCooldown)
-            else
-                wait(Settings.Timers.CheckCooldown)
-            end
-        end
-    end
-
     -- Function to toggle script enable/disable
     local function ToggleScript()
         Settings.Toggles.ScriptEnabled = not Settings.Toggles.ScriptEnabled
         if Settings.Toggles.ScriptEnabled then
-            ActivityNotification("Script Enabled", "Press U to toggle the script off")
+            ConsoleNotification("Enabled", "Script is enabled. Press U to toggle the script off")
+            ActivityNotification("Enabled", "Script is enabled. Press U to toggle the script off")
         else
-            ActivityNotification("Script Disabled", "Press U to toggle the script on")
+            Settings.Toggles.ScriptEnabled = not Settings.Toggles.ScriptEnabled
+            ConsoleNotification("Disabled", "Script is disabled. Press U to toggle the script on")
+            ActivityNotification("Disabled", "Script is disabled. Press U to toggle the script on")
+            Settings.Toggles.ScriptEnabled = not Settings.Toggles.ScriptEnabled
         end
     end
 
     -- Notify script loading and initial Settings
-    warn("Av / Hari")
+    warn("Made by Av & Hari")
     warn("Loaded script Version " .. tostring(Settings.Versions.ScriptVersion))
     warn("Works on game Version " .. tostring(Settings.Versions.StableGameVersion))
-    ActivityNotification("Status", "Script Has Been Loaded")
-    ActivityNotification("Kraken Auto Respawn", "Default: " .. tostring(Settings.Toggles.KrakenRespawn))
-    ActivityNotification("Black Market Auto Buy", "Default: " .. tostring(Settings.Toggles.BuyBlackMarket))
-    ActivityNotification("Script Enabled", "Script functionality is enabled.")
+    ConsoleNotification("Kraken Auto Respawn", "Default: " .. tostring(Settings.Toggles.KrakenRespawn))
+    ConsoleNotification("Black Market Auto Buy", "Default: " .. tostring(Settings.Toggles.BuyBlackMarket))
+    ConsoleNotification("Script Enabled", "Default: " .. tostring(Settings.Toggles.ScriptEnabled))
+    ActivityNotification("Script Enabled", "Script is enabled.")
+
+    -- Check if the Mouse object is available
+    local mouse = game:GetService("Players").LocalPlayer:GetMouse()
+    if not mouse then
+        warn("Mouse object is not available.")
+        return
+    end
+
+    -- Function to check if the user is typing
+    local function isTyping()
+        return game:GetService("UserInputService"):GetFocusedTextBox()
+    end
 
     -- Listen for key presses to toggle Settings and enable/disable script
+    local keybinds = {
+        Settings.Keybinds.KrakenToggle, 
+        Settings.Keybinds.BlackMarketToggle, 
+        Settings.Keybinds.ToggleScript
+    }
     UIS.InputBegan:Connect(function(input)
-        if input.KeyCode == Settings.Keybinds.KrakenToggle and Settings.Toggles.ScriptEnabled then
-            Settings.Toggles.KrakenRespawn = not Settings.Toggles.KrakenRespawn
-            ConsoleNotification("Kraken Auto Respawn is now:", Settings.Toggles.KrakenRespawn)
-            ActivityNotification("Kraken Auto Respawn", "Status: " .. tostring(Settings.Toggles.KrakenRespawn))
-        elseif input.KeyCode == Settings.Keybinds.BlackMarketToggle and Settings.Toggles.ScriptEnabled then
-            Settings.Toggles.BuyBlackMarket = not Settings.Toggles.BuyBlackMarket
-            ConsoleNotification("Black Market Auto Buy is now: ", Settings.Toggles.BuyBlackMarket)
-            ActivityNotification("Black Market Auto Buy", "Status: " .. tostring(Settings.Toggles.BuyBlackMarket))
-        elseif input.KeyCode == Settings.Keybinds.ToggleScript then
-            ToggleScript()
+        -- Check if the input matches any of the keybinds
+        for _, keybind in ipairs(keybinds) do
+            if input.KeyCode == keybind then
+                -- Check if the user is typing
+                if isTyping() then
+                    warn("Chat box is focused and typing, ignoring keybinds.")
+                    return -- Exit the loop and function early if the user is typing
+                end
+
+                -- Process key press if the user is not typing
+                if keybind == Settings.Keybinds.KrakenToggle and Settings.Toggles.ScriptEnabled then
+                    Settings.Toggles.KrakenRespawn = not Settings.Toggles.KrakenRespawn
+                    ConsoleNotification("Kraken Auto Respawn is now", Settings.Toggles.KrakenRespawn)
+                    ActivityNotification("Kraken Auto Respawn", "Status: " .. tostring(Settings.Toggles.KrakenRespawn))
+                elseif keybind == Settings.Keybinds.BlackMarketToggle and Settings.Toggles.ScriptEnabled then
+                    Settings.Toggles.BuyBlackMarket = not Settings.Toggles.BuyBlackMarket
+                    ConsoleNotification("Black Market Auto Buy is now", Settings.Toggles.BuyBlackMarket)
+                    ActivityNotification("Black Market Auto Buy",
+                        "Status: " .. tostring(Settings.Toggles.BuyBlackMarket))
+                elseif keybind == Settings.Keybinds.ToggleScript then
+                    ToggleScript()
+                end
+            end
         end
     end)
 
@@ -161,4 +176,4 @@ local function InjectedScript()
     coroutine.wrap(RespawnKraken)()
 end
 
-InjectedScript()  -- Call the encapsulated script function to execute the code
+Script() -- Call the encapsulated script function to execute the code
