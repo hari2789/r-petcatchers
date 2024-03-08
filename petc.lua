d0af63f5605762d7840df63d8330e38b3e497b1b
vyworn <153512249+vyworn@users.noreply.github.com>
2024-03-08 12:22:37 +0100

    Update petc.lua

--- petc.lua
+++ petc.lua
@@ -1,4 +1,16 @@
--- V1.1
+-- Version 1.3
+
+-- Script settings
+getgenv().Settings = {
+  KrakenRespawn = false,
+  Keybinds = {
+      KrakenToggle = Enum.KeyCode.R
+  }
+}
+
+-- Defining GetService
+local UIS = game:GetService("UserInputService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 -- Function to send notifications
 local function KrakenRespawnNotification(title, text)
@@ -9,33 +21,21 @@ local function KrakenRespawnNotification(title, text)
   })
 end
 
--- Script settings
-getgenv().Settings = {
-  KrakenRespawn = false,
-  Keybinds = {
-      KrakenToggle = Enum.KeyCode.R
-  }
-}
-
--- Defining variables
-local UIS = game:GetService("UserInputService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Status = tostring(Settings.KrakenRespawn)
-
 -- Notify when the script is loaded
+print("Loaded")
 KrakenRespawnNotification("Status", "Script Has Been Loaded")
-KrakenRespawnNotification("Kraken Auto Respawn", "Default: " .. Status)
+KrakenRespawnNotification("Kraken Auto Respawn", "Default: " .. tostring(Settings.KrakenRespawn))
 
 -- Listen for key presses
 UIS.InputBegan:Connect(function(input)
-  -- Check if the pressed key is the toggle key
+  -- Check if the input is the toggle key
   if input.KeyCode == Settings.Keybinds.KrakenToggle then
       -- Toggle the script state
       Settings.KrakenRespawn = not Settings.KrakenRespawn
-      -- Update the status
-      Status = tostring(Settings.KrakenRespawn)
-      -- Notify the user about the script state change
-      KrakenRespawnNotification("Kraken Auto Respawn", "Default: " .. Status)
+      -- Log the change
+      print("Kraken Auto Respawn is now:", Settings.KrakenRespawn)
+      -- Send notification about state
+      KrakenRespawnNotification("Kraken Auto Respawn", "Default: " .. tostring(Settings.KrakenRespawn))
   end
 end)
 
@@ -44,6 +44,8 @@ while true do
   if Settings.KrakenRespawn then
       -- Respawn Kraken
       ReplicatedStorage:WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild("Remote"):WaitForChild("Event"):FireServer("RespawnBoss", "the-kraken")
+      -- Log Kraken respawn
+      print("Respawned")
       task.wait(4)
   else
       task.wait(1)
